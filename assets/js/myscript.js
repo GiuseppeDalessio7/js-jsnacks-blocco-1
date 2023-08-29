0f578558f3d97dc59017003e4b253f307e34a8c0
GiuseppeDalessio7 <g.dalessio03@gmail.com>
2023-08-29 15:59:43 +0200

    add snack 3

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -2,40 +2,56 @@
 L’utente inserisce due numeri in successione, con due prompt.
 Il software stampa il maggiore.*/
 
-const number1 = Number(prompt('Inserisci il primo numero'));
+// const number1 = Number(prompt('Inserisci il primo numero'));
 
-const number2 = Number(prompt('Inserisci il primo numero'));
+// const number2 = Number(prompt('Inserisci il primo numero'));
 
 
 
-if (number1 > number2) {
-    // console.log('il numero maggiore è ' + number1);
-    let printNumber = document.getElementById('printNumber').innerHTML = ('Il numero maggiore è: ' + number1)
-} if (number2 > number1) {
-    // console.log('il numero maggiore è ' + number2);
-    let printNumber = document.getElementById('printNumber').innerHTML = ('Il numero maggiore è: ' + number2)
-} else if (number1 === number2) {
-    let printNumber = document.getElementById('printNumber').innerHTML = ('Riprova, i numeri sono uguali')
-}
+// if (number1 > number2) {
+//     // console.log('il numero maggiore è ' + number1);
+//     let printNumber = document.getElementById('printNumber').innerHTML = ('Il numero maggiore è: ' + number1)
+// } if (number2 > number1) {
+//     // console.log('il numero maggiore è ' + number2);
+//     let printNumber = document.getElementById('printNumber').innerHTML = ('Il numero maggiore è: ' + number2)
+// } else if (number1 === number2) {
+//     let printNumber = document.getElementById('printNumber').innerHTML = ('Riprova, i numeri sono uguali')
+// }
 
 /* Snack 2:
 L’utente inserisce due parole in successione, con due prompt.
 Il software stampa prima la parola più corta, poi la parola più lunga.*/
 
-const word1 = (prompt('Inserisci la prima parola'));
+// const word1 = (prompt('Inserisci la prima parola'));
 
-const word2 = (prompt('Inserisci la seconda parola'));
+// const word2 = (prompt('Inserisci la seconda parola'));
 
 
-if (word1.length > word2.length) {
-    let printWord = document.getElementById('printWordMin').innerHTML = word2
-    let printWord2 = document.getElementById('printWordMax').innerHTML = word1 ;
-} else if (word2.length > word1.length) {
-    let printWord = document.getElementById('printWordMin').innerHTML = word1
-    let printWord2 = document.getElementById('printWordMax').innerHTML = word2 ;
-} else 
-{
-    let printWord = document.getElementById('printWordMin').innerHTML = 'le parole sono uguali riprova'
-    console.log('le parole sono uguali riprova');
+// if (word1.length > word2.length) {
+//     let printWord = document.getElementById('printWordMin').innerHTML = word2
+//     let printWord2 = document.getElementById('printWordMax').innerHTML = word1 ;
+// } else if (word2.length > word1.length) {
+//     let printWord = document.getElementById('printWordMin').innerHTML = word1
+//     let printWord2 = document.getElementById('printWordMax').innerHTML = word2 ;
+// } else 
+// {
+//     let printWord = document.getElementById('printWordMin').innerHTML = 'le parole sono uguali riprova'
+//     console.log('le parole sono uguali riprova');
+// }
+
+
+/* Snack 3:
+Il software deve chiedere per 10 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti.*/
+
+let inputNumberUser;
+let somma = 0 ;
+
+for (let i = 0; i < 10; i++) {
+  inputNumberUser = Number(prompt(`Inserisci un numero ${i+1}`));
+  console.log(inputNumberUser);
+  somma += inputNumberUser 
 }
 
+console.log(somma);
+let total = document.getElementById('sommaNumeri').innerHTML = ('La somma dei numeri è: ' + somma)
