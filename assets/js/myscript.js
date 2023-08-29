7439af1491a527794364b75b37d2d73a7395c255
GiuseppeDalessio7 <g.dalessio03@gmail.com>
2023-08-29 14:44:36 +0200

    Finish Number 1 Snack

---
+++ assets/js/myscript.js
@@ -0,0 +1,21 @@
+/* Snack 1:
+L’utente inserisce due numeri in successione, con due prompt.
+Il software stampa il maggiore.*/
+
+const number1 = Number(prompt('Inserisci il primo numero'));
+console.log(number1);
+const number2 = Number(prompt('Inserisci il primo numero'));
+console.log(number2);
+
+
+if (number1 > number2) {
+    console.log('il numero maggiore è ' + number1);
+    let printNumber = document.getElementById('printNumber').innerHTML = ('Il numero maggiore è: ' + number1)
+} if (number2 > number1) {
+    console.log('il numero maggiore è ' + number2);
+    let printNumber = document.getElementById('printNumber').innerHTML = ('Il numero maggiore è: ' + number2)
+} else if (number1 === number2)
+{
+    let printNumber = document.getElementById('printNumber').innerHTML = ('Riprova, i numeri sono uguali')
+}
+
